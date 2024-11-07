38402673b7a35f8048843a8a194fc23737c3a406
Sebastian Luque <sebastian.luque.pl@gmail.com>
2024-11-06 22:07:04 -0500

    add nextjs optional plugin

--- src/flat-react.ts
+++ src/flat-react.ts
@@ -2,56 +2,66 @@ import type { FlatConfig } from "@typescript-eslint/utils/ts-eslint";
 import pluginReact from "eslint-plugin-react";
 import jsxA11y from "eslint-plugin-jsx-a11y";
 import tseslint from "typescript-eslint";
+import nextPlugin from "@next/eslint-plugin-next";
 import pluginTailwindcss from "eslint-plugin-tailwindcss";
 import { FlatCompat } from "@eslint/eslintrc";
-// @ts-expect-error - No types
 import reactHooks from "eslint-plugin-react-hooks";
 
 const compat = new FlatCompat();
 
-export const react = tseslint.config(
-  {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    extends: [
-      // @ts-expect-error - Types are broken in eslint-plugin-react https://github.com/jsx-eslint/eslint-plugin-react/issues/3838
-      pluginReact.configs.flat.recommended,
-      // @ts-expect-error - See above
-      pluginReact.configs.flat["jsx-runtime"],
-      ...compat.config(reactHooks.configs.recommended),
-      ...pluginTailwindcss.configs["flat/recommended"],
-    ],
-    languageOptions: {
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-    },
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-    rules: {
-      "react/prop-types": "off",
-      "react/display-name": "warn",
-      "react/button-has-type": "warn",
-      "tailwindcss/no-custom-classname": [
-        "warn",
-        {
-          callees: ["classnames", "clsx", "cva", "twMerge", "cn"],
-          ignoredKeys: ["color", "variant", "size", "defaultVariants"],
-        },
+export interface ReactPluginOptions {
+  framework?: "next";
+}
+
+console.log(nextPlugin.configs["core-web-vitals"]);
+
+const eslintNext = compat.config(nextPlugin.configs["core-web-vitals"]);
+
+export const react = ({ framework }: ReactPluginOptions) =>
+  tseslint.config(
+    {
+      files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+      extends: [
+        // @ts-expect-error - Types are broken in eslint-plugin-react https://github.com/jsx-eslint/eslint-plugin-react/issues/3838
+        pluginReact.configs.flat.recommended,
+        // @ts-expect-error - See above
+        pluginReact.configs.flat["jsx-runtime"],
+        ...compat.config(reactHooks.configs.recommended),
+        ...pluginTailwindcss.configs["flat/recommended"],
+        ...(framework === "next" ? eslintNext : []),
       ],
+      languageOptions: {
+        parserOptions: {
+          ecmaFeatures: {
+            jsx: true,
+          },
+        },
+      },
+      settings: {
+        react: {
+          version: "detect",
+        },
+      },
+      rules: {
+        "react/prop-types": "off",
+        "react/display-name": "warn",
+        "react/button-has-type": "warn",
+        "tailwindcss/no-custom-classname": [
+          "warn",
+          {
+            callees: ["classnames", "clsx", "cva", "twMerge", "cn"],
+            ignoredKeys: ["color", "variant", "size", "defaultVariants"],
+          },
+        ],
+      },
     },
-  },
-  {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    ignores: [
-      "**/*.stories.@(ts|tsx|js|jsx|mjs|cjs)",
-      "**/*.test.@(ts|tsx|js|jsx|mjs|cjs)",
-      "**/*.spec.@(ts|tsx|js|jsx|mjs|cjs)",
-    ],
-    extends: [jsxA11y.flatConfigs.recommended],
-  }
-) satisfies FlatConfig.ConfigArray;
+    {
+      files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+      ignores: [
+        "**/*.stories.@(ts|tsx|js|jsx|mjs|cjs)",
+        "**/*.test.@(ts|tsx|js|jsx|mjs|cjs)",
+        "**/*.spec.@(ts|tsx|js|jsx|mjs|cjs)",
+      ],
+      extends: [jsxA11y.flatConfigs.recommended],
+    }
+  ) satisfies FlatConfig.ConfigArray;
