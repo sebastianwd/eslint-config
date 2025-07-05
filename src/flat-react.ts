0aa23b579d7b6df2beb19c90b41dbf22e24bcf01
Sebastian L. <sebastian.luque.pl@gmail.com>
2025-07-05 18:08:21 -0500

    new tailwind plugin, add tanstack (#23)

--- src/flat-react.ts
+++ src/flat-react.ts
@@ -1,43 +1,38 @@
 import { FlatCompat } from '@eslint/eslintrc'
 import nextPlugin from '@next/eslint-plugin-next'
+import pluginQuery from '@tanstack/eslint-plugin-query'
 import eslintPluginAstro from 'eslint-plugin-astro'
+import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss'
 import jsxA11y from 'eslint-plugin-jsx-a11y'
 import pluginReact, { type ReactFlatConfig } from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
-import pluginTailwindcss from 'eslint-plugin-tailwindcss'
 import tseslint, { type ConfigArray } from 'typescript-eslint'
-
-import { findTailwindImportCss } from './utils/find-tailwind-config'
 
 const compat = new FlatCompat()
 
 export interface ReactPluginOptions {
   framework?: 'next' | 'expo' | 'astro'
-  tailwind?:
-    | {
-        ignoredKeys?: string[]
-      }
-    | false
+  tanstack?: boolean
 }
 
 const eslintNext = () => compat.config(nextPlugin.configs['core-web-vitals'])
 const eslintExpo = () => compat.extends('eslint-config-expo')
 const eslintAstro = eslintPluginAstro.configs.recommended
 
-const eslintTailwind = pluginTailwindcss.configs['flat/recommended']
+export const react = (options: ReactPluginOptions = {}): ConfigArray => {
+  const { framework, tanstack = true } = options
 
-export const react = (options?: ReactPluginOptions): ConfigArray =>
-  tseslint.config(
+  return tseslint.config(
     {
       files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
       extends: [
         pluginReact.configs.flat.recommended!,
         pluginReact.configs.flat['jsx-runtime'] as ReactFlatConfig,
         ...compat.config(reactHooks.configs.recommended),
-        ...(options?.framework === 'expo' ? eslintExpo() : []),
-        ...(options?.framework === 'next' ? eslintNext() : []),
-        ...(options?.framework === 'astro' ? eslintAstro : []),
-        ...(options?.tailwind === false ? [] : eslintTailwind),
+        ...(tanstack ? pluginQuery.configs['flat/recommended'] : []),
+        ...(framework === 'expo' ? eslintExpo() : []),
+        ...(framework === 'next' ? eslintNext() : []),
+        ...(framework === 'astro' ? eslintAstro : []),
       ],
       languageOptions: {
         parser: tseslint.parser,
@@ -48,43 +43,24 @@ export const react = (options?: ReactPluginOptions): ConfigArray =>
           },
         },
       },
+      plugins: {
+        'better-tailwindcss': eslintPluginBetterTailwindcss,
+      },
       settings: {
         react: {
           version: 'detect',
         },
-        ...(options?.tailwind === false
-          ? {}
-          : {
-              tailwindcss: {
-                config: findTailwindImportCss(process.cwd()),
-              },
-            }),
       },
       rules: {
+        ...eslintPluginBetterTailwindcss.configs['recommended-warn']?.rules,
         'react/prop-types': 'off',
         'react/display-name': 'warn',
         'react/button-has-type': 'warn',
-        ...(options?.framework === 'next'
+        ...(framework === 'next'
           ? {
               '@next/next/no-img-element': 'off',
             }
           : {}),
-        ...(options?.tailwind === false
-          ? {}
-          : {
-              'tailwindcss/no-custom-classname': [
-                'warn',
-                {
-                  callees: ['classnames', 'clsx', 'cva', 'twMerge', 'cn'],
-                  ignoredKeys: [
-                    'color',
-                    'variant',
-                    'size',
-                    'defaultVariants',
-                  ].concat(options?.tailwind?.ignoredKeys ?? []),
-                },
-              ],
-            }),
       },
     },
     {
@@ -97,3 +73,4 @@ export const react = (options?: ReactPluginOptions): ConfigArray =>
       extends: [jsxA11y.flatConfigs.recommended],
     }
   )
+}
